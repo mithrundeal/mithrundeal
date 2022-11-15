561aa20679bee1797c7dccd07baf80e12da8c76a
Alp Akbaba <alpakbaba@icloud.com>
2022-11-15 23:27:28 +0300

    socket v1

--- src/main/kotlin/CryptoManager.kt
+++ src/main/kotlin/CryptoManager.kt
@@ -2,7 +2,9 @@ class CryptoManager {
     init {
         // Crypto Setup, Create Cert, Create Public Key
     }
-    public fun getPublicKey() {}
+    public fun getPublicKey(): String {
+        return "test_publi_key"
+    }
     public fun encryption(data:String) {}
     public fun decryption(publicKey:String, data:String) {}
 }
--- src/main/kotlin/Mithrundeal.kt
+++ src/main/kotlin/Mithrundeal.kt
@@ -1,9 +1,123 @@
-class Mithrundeal(networkPassKey: String) {
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.*
+import java.net.ConnectException
+import java.net.NetworkInterface
+
+class Mithrundeal(val networkPassKey: String, val port: Int = 57611) {
+
+    private val dataManager: DataManager = DataManager()
+    private val cryptoManager: CryptoManager = CryptoManager()
+
+    private val selectorManager = SelectorManager(Dispatchers.IO)
+    private val serverSocket = aSocket(selectorManager).tcp().bind("0.0.0.0", port)
+
+    private val localAddress: String = detectLocalAddress()
+    private val localAddressParts = localAddress.split(".")
+    private val localAddressLastPart:Int = Integer.parseInt(localAddressParts[localAddressParts.lastIndex].toString())
+
+    private val localAddressPrefix: String = "${localAddressParts[0]}.${localAddressParts[1]}.${localAddressParts[2]}"
 
     init {
         //Server & Client Setup
+
     }
 
-    public fun start(){}
+    public fun start() {
+        runBlocking {
+            async { startServer() }
+            async { scanLocalNetwork() }
+        }
+    }
     public fun close(){}
+
+    fun startServer() = runBlocking {
+        println("Server Started!")
+        async {
+            while (true) {
+                try {
+                    val socket = serverSocket.accept()
+                    println("Accepted ${socket.localAddress}")
+                    launch {
+                        val receiveChannel = socket.openReadChannel()
+                        //val sendChannel = socket.openWriteChannel(autoFlush = true)
+
+                        //Post PublicKey
+                        //sendChannel.writeStringUtf8(cryptoManager.getPublicKey())
+
+                        // Post ClientList
+                        //sendChannel.writeStringUtf8(dataManager.getClientList().toString())
+
+                        //sendChannel.writeStringUtf8("Please enter your name\n")
+                        try {
+                            while (true) {
+                                val name = receiveChannel.readUTF8Line()
+                                println(name)
+                                //sendChannel.writeStringUtf8("Hello, $name!\n")
+                            }
+                        } catch (e: Throwable) {
+                            //socket.close()
+                        }
+                    }
+                }catch (e:Exception)
+                {
+                    println(e.message)
+                }
+            }
+        }
+    }
+
+    suspend fun connectToClient(IPLastPart: Int): Socket? {
+        val remoteClientIP = localAddressPrefix+"."+IPLastPart
+        //println("Trying connect $remoteClientIP")
+        try {
+            val socket = aSocket(selectorManager).tcp().connect(remoteClientIP, port)
+            println("Trying connect /$remoteClientIP:$port")
+            val sendChannel = socket.openWriteChannel(autoFlush = true)
+            sendChannel.writeStringUtf8("test write")
+
+            return socket
+        }catch (e: ConnectException) {
+            //println(e.message)
+        }
+
+        return null
+    }
+
+    private val connections: MutableList<Deferred<Socket?>> = mutableListOf()
+    private fun scanLocalNetwork() = runBlocking{
+
+        for (x in 1..254) {
+
+            /*if(x == localAddressLastPart)
+                    continue*/
+
+            //connections.add()
+            val newSocket: Deferred<Socket?> = async<Socket?> {
+                val socket:Socket? = connectToClient(x)
+                return@async socket
+            }
+            newSocket.invokeOnCompletion {
+                //connections.remove(newSocket)
+            }
+            connections.add(newSocket)
+        }
+
+    }
+
+    private fun detectLocalAddress():String{
+        for (networkInterface in NetworkInterface.getNetworkInterfaces()) {
+            if(networkInterface.interfaceAddresses.size > 0){
+                for (interfaceAddress in networkInterface.interfaceAddresses) {
+                    if(interfaceAddress.address.hostAddress.contains("192.168"))
+                    {
+                        return interfaceAddress.address.hostAddress
+                        break;
+                    }
+                }
+            }
+        }
+        return String()
+    }
 }

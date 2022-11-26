e7145c8614e6bf0f9701a76ce89c504db44f0620
alp-akbaba <erdemakbaba@172.16.1.250>
2022-11-26 15:05:01 +0300

    ktor deleted.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,14 @@
 import crypto.CryptoInit
-import crypto.Drbg
-import crypto.RSAHelper
-
-import java.security.Provider
-import java.security.SecureRandom
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider
-import org.bouncycastle.util.encoders.Hex
 import java.security.KeyPair
-import java.security.Security
-import java.util.*
-
 
 lateinit var rsaKeyPair: KeyPair
 fun main(args: Array<String>) {
 
-    runBlocking {
-        async {
-            val mithrundealNetwork: Mithrundeal = Mithrundeal("my-network-secret-separator")
-            mithrundealNetwork.start()
-        }
-    }
+    val mithrundealNetwork: Mithrundeal = Mithrundeal("my-network-secret-separator")
+    mithrundealNetwork.start()
 
-    rsaKeyPair = RSAHelper.getKeyPair()
+
+    /*rsaKeyPair = RSAHelper.getKeyPair()
 
 
     val keyPair = RSAHelper.getKeyPair()
@@ -38,7 +23,7 @@ fun main(args: Array<String>) {
     println(verify.toString())
 
     println(Hex.toHexString(rsaKeyPair.private.encoded))
-    println(Hex.toHexString(keyPair.private.encoded))
+    println(Hex.toHexString(keyPair.private.encoded))*/
 
 
 }
